2a31af190a82b0895950b81c2947212db7bd2c51
Wayne Gabriel Nunes Ferreira <27884@gruposc.local>
2022-11-11 12:11:30 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,49 @@
+let display = document.getElementById('display');
+
+        let hour = 0;
+        let min = 0;
+        let sec = 0;
+        let interval;
+
+        function start() {
+            let start = document.getElementById('start');
+            start.style.display = 'none'
+            let pause = document.getElementById('pause')
+            pause.style.display = 'inline-flex'
+            interval = setInterval(() => {
+                sec++
+                if (sec == 60) {
+                    min++
+                    sec = 0
+                    if (min == 60) {
+                        hour++
+                        min = 0
+                    }
+                }
+
+                display.innerHTML = (hour < 10 ? '0' + hour : hour) + ':'
+                    + (min < 10 ? '0' + min : min) + ':'
+                    + (sec < 10 ? '0' + sec : sec)
+            }, 1000)
+        }
+
+        function pause() {
+            let start = document.getElementById('start');
+            start.style.display = 'inline-flex'
+            let pause = document.getElementById('pause')
+            pause.style.display = 'none'
+            clearInterval(interval)
+        }
+
+        function stop() {
+            clearInterval(interval)
+            hour = 0;
+            min = 0;
+            sec = 0;
+            display.innerHTML = '00:00:00'
+            let start = document.getElementById('start');
+            start.style.display = 'inline-flex'
+            let pause = document.getElementById('pause')
+            pause.style.display = 'none'
+
+        }
