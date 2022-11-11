ced04e18a0a3ba79f73e9352b23002e9a61a7783
Wayne Gabriel Nunes Ferreira <27884@gruposc.local>
2022-11-11 12:43:57 -0300

    adjustments

--- main.js
+++ main.js
@@ -28,11 +28,37 @@ let display = document.getElementById('display');
         }
 
         function pause() {
-            let start = document.getElementById('start');
-            start.style.display = 'inline-flex'
             let pause = document.getElementById('pause')
             pause.style.display = 'none'
+            let continuar = document.getElementById('continue')
+            continuar.style.display = 'inline-flex'
             clearInterval(interval)
+        }
+
+        function continuar(){
+            let continuar = document.getElementById('continue')
+            continuar.style.display = 'none'
+            let pause = document.getElementById('pause')
+            pause.style.display = 'inline-flex'
+            
+
+
+            interval = setInterval(() => {
+                sec++
+                if (sec == 60) {
+                    min++
+                    sec = 0
+                    if (min == 60) {
+                        hour++
+                        min = 0
+                    }
+                }
+
+                display.innerHTML = (hour < 10 ? '0' + hour : hour) + ':'
+                    + (min < 10 ? '0' + min : min) + ':'
+                    + (sec < 10 ? '0' + sec : sec)
+            }, 1000)
+
         }
 
         function stop() {
@@ -45,5 +71,7 @@ let display = document.getElementById('display');
             start.style.display = 'inline-flex'
             let pause = document.getElementById('pause')
             pause.style.display = 'none'
+            let continuar = document.getElementById('continue')
+            continuar.style.display = 'none'           
 
         }
